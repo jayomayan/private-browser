f81d699e769223b81042ecd17764afb3232673ba
Jay Omayan <jomayan@gmail.com>
2025-04-03 19:54:55 -0700

    Update PrivateBrowser.php

--- app/Livewire/PrivateBrowser.php
+++ app/Livewire/PrivateBrowser.php
@@ -44,10 +44,15 @@ class PrivateBrowser extends Component
         $this->pingresponse = $this->pingIP($this->privateIp);
 
         try {
-            Browsershot::url('http://10.194.80.240')
-    ->setOption('args', ['--no-sandbox'])
-    ->windowSize(1280, 720)
-    ->save(storage_path('app/public/snapshots/10.194.80.240.png'));
+            Browsershot::url("http://{$this->privateIp}")
+                ->setTakesScreenshot()
+                ->setQuality(100)
+                ->setDelay(500)
+                ->setScreenshotType('png')
+                ->setChromePath('/snap/bin/chromium')
+                ->setOption('args', ['--no-sandbox'])
+                ->windowSize(1280, 720)
+                ->save(storage_path("app/public/snapshots/{$this->privateIp}.png"));
         } catch (\Exception $e) {
             logger()->error('Browsershot failed: ' . $e->getMessage());
         }
