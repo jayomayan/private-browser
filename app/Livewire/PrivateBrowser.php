1be982f2073e444f9e636e5d9a5bfd2bcbf814f3
Jay Omayan <jomayan@gmail.com>
2025-05-10 13:23:42 -0700

    Update PrivateBrowser.php

--- app/Livewire/PrivateBrowser.php
+++ app/Livewire/PrivateBrowser.php
@@ -188,7 +188,7 @@ class PrivateBrowser extends Component
     if ($deviceType === 'type3') {
         try {
             $firmware = Browsershot::url("https://$ip/")
-            ->setChromeExecutablePath('/var/www/.cache/puppeteer/chrome/linux-*/chrome')
+            ->setChromeExecutablePath('/var/www/.cache/puppeteer/chrome/linux-135.0.7049.42/chrome')
             ->setNodeBinary('/usr/bin/node')
             ->setNpmBinary('/usr/bin/npm')
             ->waitUntilNetworkIdle()
