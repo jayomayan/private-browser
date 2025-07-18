1f446464f92ec398939c233c671b79b1e78be318
Jay Omayan <jomayan@gmail.com>
2025-07-18 00:09:32 -0700

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,11 +1,16 @@
 <?php
 
+
+use Illuminate\Support\Facades\Log;
+
 if (!function_exists('download_logs')) {
 function download_logs($ip, $username = 'admin', $password = 'admin')
 {
     $scriptPath = base_path('node-scripts/download-logs.cjs');
 
     $command = "node $scriptPath $ip $username $password";
+
+    Log::info($scriptPath);
 
     // 🔍 Add this line to log what Laravel is actually running:
     file_put_contents(storage_path('logs/download-command.log'), $command . PHP_EOL, FILE_APPEND);
