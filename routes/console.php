0fc7471811dfc5a8fd32df2054014f54b56c62e8
Jay Omayan <jomayan@gmail.com>
2025-08-01 03:08:56 -0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 return function (Schedule $schedule) {
-    $schedule->command('logs:download')->everyFifteenMinutes();
+    $schedule->command('logs:download')
+             ->everyFifteenMinutes()
+             ->withoutOverlapping(30);
 };
-
