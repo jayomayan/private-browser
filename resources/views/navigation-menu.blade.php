9962e0081b30be99f924cb99bca6342466cb42fa
Jay Omayan <jomayan@gmail.com>
2025-08-19 22:00:06 -0700

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -26,6 +26,11 @@
                         {{ __('SNMP Walk') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('device-ntp') }}" :active="request()->routeIs('device-ntp')">
+                        {{ __('Device NTP') }}
+                    </x-nav-link>
+                </div>
 
             </div>
 
