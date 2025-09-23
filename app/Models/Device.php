e2f331c6da4717a393afffdd13ceb8dc58591559
Jay Omayan <jomayan@gmail.com>
2025-09-23 21:39:48 +0800

    Update Device.php

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -6,7 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-  protected $fillable = ['ip', 'name', 'site_id']; // allow mass assignment
+   protected $fillable = [
+        'ip',
+        'name',
+        'site_id',
+        'arm_version',
+        'stm32_version',
+        'web_version',
+        'kernel_version',
+        'mib_version',
+    ];
+    // allow mass assignment
   protected $casts = ['last_log_pulled_at' => 'datetime',];
 
     // Optional: define relationship if you want
